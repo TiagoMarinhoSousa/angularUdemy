52b5a199584e4030adf4e8532beaa8f013a385f0
TiagoMarinhoSousa <tiagomarinho9101@gmail.com>
2022-11-03 17:13:02 -0300

    Final do modulo 3 - Componentes

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-title *ngIf="destruir"></app-title>
+    <br>
+    <button (click)="destruirComponente()">Destruir Componente</button>
+    <!-- {{ valor }}
+    <button (click)="adicionar()">Adicionar</button> -->
+    <router-outlet></router-outlet>
+    `
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto';
+export class AppComponent implements OnInit {
+
+
+  constructor() {
+
+   }
+   public destruir:boolean = true;
+   public destruirComponente(){
+    this.destruir = false;
+   }
+  // public valor: number = 1;
+  //  public adicionar():number {
+  //   return this.valor += 1;
+  //  }
+
+  ngOnInit(): void { }
+
+
+  // ngDoCheck(): void {
+  //   console.log('ngDoCheck');
+  // }
+  // ngAfterContentChecked(): void {
+  //   console.log('ngAfterContentChecked');
+  // }
+  // ngAfterContentInit(): void {
+  //   console.log('ngAfterContentInit');
+  // }
+  // ngAfterViewChecked(): void {
+  //   console.log('ngAfterViewChecked');
+  // }
+  // ngAfterViewInit(): void {
+  //   console.log('ngAfterViewInit');
+  // }
 }
