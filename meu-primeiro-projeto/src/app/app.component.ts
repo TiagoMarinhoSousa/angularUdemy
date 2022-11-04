7523a762d2b69faa92cb60dfa893bd898920e7be
TiagoMarinhoSousa <tiagomarinho9101@gmail.com>
2022-11-04 16:51:22 -0300

    Fim do modulo data-binding

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,55 +1,13 @@
-import {
-  AfterContentChecked,
-  AfterContentInit,
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  DoCheck,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-title *ngIf="destruir"></app-title>
-    <br>
-    <button (click)="destruirComponente()">Destruir Componente</button>
-    <!-- {{ valor }}
-    <button (click)="adicionar()">Adicionar</button> -->
-    <router-outlet></router-outlet>
-    `
+    <app-data-binding></app-data-binding>
+    <router-outlet></router-outlet> `,
 })
 export class AppComponent implements OnInit {
+  constructor() {}
 
-
-  constructor() {
-
-   }
-   public destruir:boolean = true;
-   public destruirComponente(){
-    this.destruir = false;
-   }
-  // public valor: number = 1;
-  //  public adicionar():number {
-  //   return this.valor += 1;
-  //  }
-
-  ngOnInit(): void { }
-
-
-  // ngDoCheck(): void {
-  //   console.log('ngDoCheck');
-  // }
-  // ngAfterContentChecked(): void {
-  //   console.log('ngAfterContentChecked');
-  // }
-  // ngAfterContentInit(): void {
-  //   console.log('ngAfterContentInit');
-  // }
-  // ngAfterViewChecked(): void {
-  //   console.log('ngAfterViewChecked');
-  // }
-  // ngAfterViewInit(): void {
-  //   console.log('ngAfterViewInit');
-  // }
+  ngOnInit(): void {}
 }
